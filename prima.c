2a722843e713194a4a7cfa77d6e3a1de7486f3a2
Wojtyfa <benedictus.karol@ti.ukdw.ac.id>
2024-10-10 14:26:16 +0700

    coba

--- prima.c
+++ prima.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     int num, i, isPrime = 1;
 
-    printf("Masukkan bilangan: ");
+    printf("Masukkan sebuah bilangan: ");
     scanf("%d", &num);
 
-    // Cek apakah bilangan lebih kecil dari 2
+    // Bilangan prima lebih besar dari 1
     if (num <= 1) {
         isPrime = 0;
     } else {
-        // Cek pembagi dari 2 hingga akar kuadrat dari bilangan
-        for (i = 2; i <= sqrt(num); ++i) {
+        // Cek apakah bilangan habis dibagi bilangan selain 1 dan dirinya sendiri
+        for (i = 2; i <= num/2; ++i) {
             if (num % i == 0) {
                 isPrime = 0;
                 break;
@@ -20,11 +19,11 @@ int main() {
         }
     }
 
-    if (isPrime) {
+    if (isPrime)
         printf("%d adalah bilangan prima.\n", num);
-    } else {
+    else
         printf("%d bukan bilangan prima.\n", num);
-    }
 
     return 0;
 }
+
